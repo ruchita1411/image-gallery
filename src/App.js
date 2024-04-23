7a09c40c2f3c43cc1a75f86397bf44d40a251a21
ruchita1411 <80165075+ruchita1411@users.noreply.github.com>
2024-04-23 16:26:05 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import { useRef } from "react";
 
 function App() {
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1027/1000/600/",
+      thumbnail: "https://picsum.photos/id/1027/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1016/1000/600/",
+      thumbnail: "https://picsum.photos/id/1016/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1020/1000/600/",
+      thumbnail: "https://picsum.photos/id/1020/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1028/1000/600/",
+      thumbnail: "https://picsum.photos/id/1028/250/150/",
+    },
+  ];
+  const slideshowref = useRef();
+
+  function playSlides() {
+    slideshowref.current.play();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageGallery
+        items={images}
+        showPlayButton={false}
+        showFullscreenButton={false}
+        slideInterval={1000}
+        slideOnThumbnailOver={true}
+        showIndex={true}
+        onPlay={() => {
+          alert("slideshow is playing!");
+        }}
+      />
+      <button onClick={playSlides}>Play</button>
     </div>
   );
 }
